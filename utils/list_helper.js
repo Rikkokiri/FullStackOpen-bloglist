9d7195abc2848d7d20aea087d3387f8c7adab403
Ossian Rajala <ossian.rajala@gmail.com>
2022-04-07 13:36:10 +0300

    4.3: configure jest and test dummy function

---
+++ utils/list_helper.js
@@ -0,0 +1,11 @@
+/** Define a dummy function that receives an array of blog posts as a parameter
+ * and always returns the value 1.
+ */
+const dummy = (blogs) => {
+  // ...
+  return 1;
+};
+
+module.exports = {
+  dummy,
+};
