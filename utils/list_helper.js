b763edbc9a8b01421ae7394bb89ebc2b2499bc7c
Ossian Rajala <ossian.rajala@gmail.com>
2022-04-07 14:35:53 +0300

    4.5: implement favoriteBlog

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -13,7 +13,28 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, curr) => sum + curr.likes, 0);
 };
 
+/**
+ * Define a new favoriteBlog function that receives a list of blogs as a parameter.
+ * The function finds out which blog has most likes. If there are many top favorites,
+ * it is enough to return one of them.
+ */
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return undefined;
+
+  const favorite = blogs.reduce((prev, curr) =>
+    prev.likes > curr.likes ? prev : curr
+  );
+
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes,
+  };
+};
+
 module.exports = {
   dummy,
+  favoriteBlog,
   totalLikes,
 };
