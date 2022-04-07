e6abccc5faa00614cb923505e40ffb3de71d79b1
Ossian Rajala <ossian.rajala@gmail.com>
2022-04-07 14:01:39 +0300

    test: use provided test data

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -1,4 +1,5 @@
 const { expect } = require('@jest/globals');
+const { describe } = require('yargs');
 const listHelper = require('../utils/list_helper');
 
 test('dummy returns one', () => {
@@ -8,61 +9,69 @@ test('dummy returns one', () => {
   expect(result).toBe(1);
 });
 
+const listWithOneBlog = [
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0,
+  },
+];
+
+const blogs = [
+  {
+    _id: '5a422a851b54a676234d17f7',
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+    __v: 0,
+  },
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0,
+  },
+  {
+    _id: '5a422b3a1b54a676234d17f9',
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 12,
+    __v: 0,
+  },
+  {
+    _id: '5a422b891b54a676234d17fa',
+    title: 'First class tests',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+    likes: 10,
+    __v: 0,
+  },
+  {
+    _id: '5a422ba71b54a676234d17fb',
+    title: 'TDD harms architecture',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+    likes: 0,
+    __v: 0,
+  },
+  {
+    _id: '5a422bc61b54a676234d17fc',
+    title: 'Type wars',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+    likes: 2,
+    __v: 0,
+  },
+];
+
 describe('total likes', () => {
-  const listWithOneBlog = [
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 5,
-      __v: 0,
-    },
-  ];
-
-  const listWithMultipleBlogs = [
-    {
-      _id: '624eb173cbc394158e5dd221',
-      title: 'Sample Blog Post',
-      author: 'Test Blogger',
-      url: 'www.blog.test.com/sample-blog',
-      likes: 1,
-      __v: 0,
-    },
-    {
-      _id: '624eb1b7cbc394158e5dd224',
-      title: 'Best Blog Post',
-      author: 'Best Blogger',
-      url: 'www.bestblog.com/best-post-ever',
-      likes: 10,
-      __v: 0,
-    },
-    {
-      _id: '624eb3c848e06ea986ff858a',
-      title: 'Wors Blog Post',
-      author: 'Wors Blogger',
-      url: 'www.worstblog.com/this-is-the-worst',
-      likes: 55,
-      __v: 0,
-    },
-    {
-      _id: '624eb3d748e06ea986ff858c',
-      title: 'Worst Blog Post',
-      author: 'Worst Blogger',
-      url: 'www.worstblog.com/this-is-the-worst',
-      likes: 0,
-      __v: 0,
-    },
-    {
-      _id: '624eb6517442dbd306e92837',
-      title: 'Some post',
-      author: 'Some Blogger',
-      url: 'www.someblog.com/just-a-random-post',
-      likes: 4,
-      __v: 0,
-    },
-  ];
-
   test('of empty list is zero', () => {
     expect(listHelper.totalLikes([])).toBe(0);
   });
@@ -72,6 +81,6 @@ describe('total likes', () => {
   });
 
   test('of a bigger list is calculated right', () => {
-    expect(listHelper.totalLikes(listWithMultipleBlogs)).toBe(70);
+    expect(listHelper.totalLikes(blogs)).toBe(36);
   });
 });
