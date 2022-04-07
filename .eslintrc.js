e708da75d5e6abf0ea1294c0110e0a8bf939198d
Ossian Rajala <ossian.rajala@gmail.com>
2022-04-07 14:28:05 +0300

    chore: configure prettier and eslint to play nice together

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,19 +6,23 @@ module.exports = {
     node: true,
     jest: true,
   },
-  extends: 'eslint:recommended',
+  extends: [
+    'eslint:recommended',
+    // This always needs to remain as the last one:
+    'plugin:prettier/recommended',
+  ],
   parserOptions: {
     ecmaVersion: 'latest',
   },
   rules: {
     'arrow-spacing': ['error', { before: true, after: true }],
     eqeqeq: 'error',
-    indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
     'no-console': 0,
     'no-trailing-spaces': 'error',
     'object-curly-spacing': ['error', 'always'],
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
+    'prettier/prettier': 'error',
   },
 };
