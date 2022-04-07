6eae449c3834ed7aa4aaad1e5cc3441acc5f3b22
Ossian Rajala <ossian.rajala@gmail.com>
2022-04-07 13:31:05 +0300

    chore:  eslint config from part 3

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'arrow-spacing': ['error', { before: true, after: true }],
+    eqeqeq: 'error',
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    'no-console': 0,
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+  },
+};
