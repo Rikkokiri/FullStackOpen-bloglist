45b16323481195802d7403d0b15bf18c6888f8ff
Ossian Rajala <ossian.rajala@gmail.com>
2022-04-08 11:53:57 +0300

    test(api): group and plan tests

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -14,74 +14,90 @@ beforeEach(async () => {
   }
 });
 
-test('blog posts are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
+describe('when there is initially some blog posts saved', () => {
+  test('blog posts are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+  });
+
+  test('all blog posts are returned', async () => {
+    const response = await api.get('/api/blogs');
+    expect(response.body).toHaveLength(helper.initialBlogs.length);
+  });
+
+  test('specific blog post is within returned posts', async () => {
+    const response = await api.get('/api/blogs');
+    const titles = response.body.map((b) => b.title);
+    expect(titles).toContain('Go To Statement Considered Harmful');
+  });
+
+  test('blog identifier is called id', async () => {
+    const response = await api.get('/api/blogs');
+    const blog = response.body[0];
+    expect(blog.id).toBeDefined();
+  });
 });
 
-test('all blog posts are returned', async () => {
-  const response = await api.get('/api/blogs');
-  expect(response.body).toHaveLength(helper.initialBlogs.length);
+describe('viewing a specific blog post', () => {
+  test.skip('succeeds when id is valid', async () => {});
+
+  test.skip('fails with statuscode 404 if blog post does not exist', async () => {});
+
+  test.skip('fails with statuscode 400 id is invalid', async () => {});
 });
 
-test('specific blog post is within returned posts', async () => {
-  const response = await api.get('/api/blogs');
-  const titles = response.body.map((b) => b.title);
-  expect(titles).toContain('Go To Statement Considered Harmful');
+describe('creating a new blog post', () => {
+  test('new blog post is saved to the database', async () => {
+    const newBlog = {
+      title: 'How a Web Design Goes Straight to Hell',
+      author: 'Matthew Inman',
+      url: 'https://theoatmeal.com/comics/design_hell',
+      likes: 100,
+    };
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
+
+    const blogsAfter = await helper.blogsInDB();
+    expect(blogsAfter).toHaveLength(helper.initialBlogs.length + 1);
+
+    const titles = blogsAfter.map((b) => b.title);
+    expect(titles).toContain(newBlog.title);
+  });
+
+  test('blog likes will default to value 0', async () => {
+    const newBlog = {
+      title: 'About coding the FizzBuzz interview question',
+      author: 'Michele Riva',
+      url: 'https://micheleriva.medium.com/about-coding-the-fizzbuzz-interview-question-9bcd08d9dfe5',
+    };
+
+    const response = await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
+    expect(response.body.likes).toEqual(0);
+  });
+
+  test('blog without url and title will be rejected', async () => {
+    const newBlog = {
+      author: 'Unknown blogger',
+    };
+
+    await api.post('/api/blogs').send(newBlog).expect(400);
+    const blogsAfter = await helper.blogsInDB();
+    expect(blogsAfter).toHaveLength(helper.initialBlogs.length);
+  });
 });
 
-test('blog identifier is called id', async () => {
-  const response = await api.get('/api/blogs');
-  const blog = response.body[0];
-  expect(blog.id).toBeDefined();
-});
-
-test('new blog post is saved to the database', async () => {
-  const newBlog = {
-    title: 'How a Web Design Goes Straight to Hell',
-    author: 'Matthew Inman',
-    url: 'https://theoatmeal.com/comics/design_hell',
-    likes: 100,
-  };
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/);
-
-  const blogsAfter = await helper.blogsInDB();
-  expect(blogsAfter).toHaveLength(helper.initialBlogs.length + 1);
-
-  const titles = blogsAfter.map((b) => b.title);
-  expect(titles).toContain(newBlog.title);
-});
-
-test('blog likes will default to value 0', async () => {
-  const newBlog = {
-    title: 'About coding the FizzBuzz interview question',
-    author: 'Michele Riva',
-    url: 'https://micheleriva.medium.com/about-coding-the-fizzbuzz-interview-question-9bcd08d9dfe5',
-  };
-
-  const response = await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/);
-  expect(response.body.likes).toEqual(0);
-});
-
-test('blog without url and title will be rejected', async () => {
-  const newBlog = {
-    author: 'Unknown blogger',
-  };
-
-  await api.post('/api/blogs').send(newBlog).expect(400);
-  const blogsAfter = await helper.blogsInDB();
-  expect(blogsAfter).toHaveLength(helper.initialBlogs.length);
+describe('deleting a blog post', () => {
+  test.skip('succeeds with status code 204 if id is valid', async () => {});
 });
 
 afterAll(() => {
