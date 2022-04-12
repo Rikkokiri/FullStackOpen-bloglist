036e345636c29d133dae7ff905f65821c732f6b3
Ossian Rajala <ossian.rajala@gmail.com>
2022-04-12 14:27:35 +0300

    test(login): test suite for login router

---
+++ tests/login_api.test.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const app = require('../app');
+const api = supertest(app);
+const User = require('../models/user');
+const helper = require('./test_helper');
+
+beforeEach(async () => {
+  try {
+    // Add user to database (so there is a user that can be attached to blogs)
+    await helper.initDbWithUsers();
+  } catch (error) {
+    console.log('Error initalizing database', error);
+  }
+});
+
+describe('login', () => {
+  test('succeeds when username and password are correct', async () => {
+    const [user, ..._] = helper.initialUsers;
+    await api
+      .post('/api/login')
+      .send({ username: user.username, password: user.password })
+      .expect(200);
+  });
+
+  test('fails if username is missing', async () => {
+    const [user, ..._] = helper.initialUsers;
+    await api.post('/api/login').send({ password: user.password }).expect(401);
+  });
+
+  test('fails if password is incorrect', async () => {
+    const [user, ..._] = helper.initialUsers;
+    await api
+      .post('/api/login')
+      .send({ username: user.username, password: user.password + '123' })
+      .expect(401);
+  });
+});
+
+afterAll(() => {
+  mongoose.connection.close();
+});
